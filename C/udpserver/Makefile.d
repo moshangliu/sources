eb3a33633afb02edf56007c0a73157c7086ebd10
liangrongtang <liangrongtang@xiaomi.com>
2015-03-03 14:22:33 +0800

    1. remove thirdparty; 2. make

--- C/udpserver/Makefile.d
+++ C/udpserver/Makefile.d
@@ -3,21 +3,22 @@ builddate=$(shell date)
 
 OUTDIR=output
 
-CPPFLAGS+=-g -O3 -Wall -pipe -D_REENTRANT -pthread -DVERSION=\""$(version)"\" -DBUILD_DATE=\""$(builddate)"\"
-CC= g++
+CPPFLAGS+=-g -O3 -Wall -pipe -D_REENTRANT -DVERSION=\""$(version)"\" -DBUILD_DATE=\""$(builddate)"\"
+#CC= g++
+CC=/opt/soft/tcpsvr/gcc-4.8.1/bin/g++
 
 SRCDIR = src
 MODULES = Thread Common Main UDP
 
 INCLUDEDIR=../../include/
-LOG4CPLUS_INCLUDE_DIR=../../third_party/log4cplus-1.1.2/include/
-LOG4CPLUS_LIB_DIR=../../third_party/log4cplus-1.1.2/lib/
 
-INCLUDE_DIRS=-I$(INCLUDEDIR) -I$(LOG4CPLUS_INCLUDE_DIR)
-LIBS_DIR=-L$(LOG4CPLUS_LIB_DIR)
-LIBS=-llog4cplus
+INCLUDE_DIRS=-I$(INCLUDEDIR) \
+	 -I/opt/soft/tcpsvr/log4cplus/include
+LIBS_DIR=/opt/soft/tcpsvr/gcc-4.8.1/lib64/libstdc++.a \
+	 /opt/soft/tcpsvr/log4cplus/lib/liblog4cplus.a\
+	 -lpthread
 
-CC_COMPILE_LINE=$(CPPFLAGS) $(INCLUDE_DIRS) $(LIBS_DIR) $(LIBS)
+CC_COMPILE_LINE=$(CPPFLAGS) $(INCLUDE_DIRS) $(LIBS_DIR)
 
 %.o: %.cpp $(wildcard  $(INCLUDEDIR)/*.h)
-	@$(CC) $(CC_COMPILE_LINE) -c $< -o $@
+	@$(CC) $(CPPFLAGS) $(INCLUDE_DIRS) -c $< -o $@
